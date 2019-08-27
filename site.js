feb6204102ec8497e9c822095af789d0852e7c55
Holajuwon <36427779+Holajuwon@users.noreply.github.com>
2019-08-27 22:13:52 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,4 +1,16 @@
-alert('javascript is working')
-$('div').on('click', function () {
-    $(this).toggleClass('show-description');
-});
+// alert('javascript is working')
+// $('div').on('click', function () {
+//     $(this).toggleClass('show-description');
+// });
+
+const showDescription = () =>{
+
+    let description = document.querySelectorAll('div')
+    description.forEach(description=>{
+        description.classList.toggle('show-description')
+    })
+
+    
+}
+
+document.querySelector('div').addEventListener('click', showDescription);
